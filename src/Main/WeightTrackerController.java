e1416c2b28631d1f633158cc9598dd5c92094cbd
jessopi <ianjessop3@gmail.com>
2018-03-14 18:51:07 -0400

    Added validation of user input & ability to search and display database values in lineChart and table.

--- src/Main/WeightTrackerController.java
+++ src/Main/WeightTrackerController.java
@@ -2,13 +2,15 @@ package Main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class WeightTrackerController {
 
-    WeightTrackerModel weightTrackerModel = new WeightTrackerModel();
+    private WeightTrackerModel weightTrackerModel = new WeightTrackerModel();
 
     @FXML
     private TextField weightEntry;
@@ -22,48 +24,83 @@ public class WeightTrackerController {
     private DatePicker startingDateSearch;
     @FXML
     private DatePicker endingDateSearch;
+    @FXML
+    private TableView<WeightData> dataTable;
+    @FXML
+    private TableColumn<WeightData,String> dateCol;
+    @FXML
+    private TableColumn<WeightData,Double> weightCol;
+    @FXML
+    private LineChart<?,?> weightChart;
 
 
+    /*
+            things to do:
+                seperate code logic a bit more
+                Test linegraph with 100s of data entries to make sure it works properly (JUNIT)
+                Add labels to X & Y axis
+                add functionality to load all data with click of button
+                ability to delete entries/remove all?
+                Add information on additional tab - info about current set of data
+                have a status bar representing current databse connection.
+
+     */
     public void click(ActionEvent event){
         Button btn = (Button) event.getSource();
         String id = btn.getId();
         if(id.equals(addButton.getId())){
            if(isValidEntry()){
                System.out.println("addButton clicked");
-               weightTrackerModel.insertNewData(dateEntry.getValue(),Double.parseDouble(weightEntry.getText()));
+               weightTrackerModel.insertNewData(dateEntry.getValue().toString(),Double.parseDouble(weightEntry.getText()));
            } else {
                System.out.println("Invalid data Entry");
            }
         } else if(id.equals(searchButton.getId())){
             if(isValidSearch()){
                 System.out.println("searchButton clicked");
-                //pass into modelViewController for searching & updating of tabs
+                updateTable();
+                updateLineChart();
             } else {
                 System.out.println("Invalid data Search");
             }
         }
     }
 
-    private boolean isValidEntry(){
-        if(dateEntry.getValue() == null || weightEntry.getText().isEmpty()){
-            return false;
-        } else if(Double.parseDouble(weightEntry.getText()) < 0){
-            return false;
-        } else {
-            return true;
-        }
+    private void updateTable(){
+        this.weightTrackerModel.setSearchRange(this.startingDateSearch.getValue().toString(),this.endingDateSearch.getValue().toString());
+        this.dateCol.setCellValueFactory(new PropertyValueFactory<>("Date"));
+        this.weightCol.setCellValueFactory(new PropertyValueFactory<>("weight"));
+        this.dataTable.setItems(this.weightTrackerModel.getWeightDataList());
     }
-
+    private void updateLineChart(){
+        this.weightChart.getData().clear();
+        XYChart.Series series = new XYChart.Series();
+        NumberAxis yAxis = (NumberAxis)this.weightChart.getYAxis();
+        yAxis.setForceZeroInRange(false);
+        for(WeightData item : this.weightTrackerModel.getWeightDataList()){
+            series.getData().add(new XYChart.Data(item.getDate(),item.getWeight()));
+        }
+        this.weightChart.getData().add(series);
+    }
     private boolean isValidSearch(){
-        if(startingDateSearch.getValue() == null || endingDateSearch.getValue() == null){
+        try{
+            if(startingDateSearch.getValue() == null || endingDateSearch.getValue() == null ||endingDateSearch.getValue().isBefore(startingDateSearch.getValue())){
+                throw new IllegalArgumentException("Illegal values entered");
+            }
+        } catch (IllegalArgumentException ex) {
+            System.out.println("CAUGHT");
             return false;
-        } else if(endingDateSearch.getValue().isBefore(startingDateSearch.getValue())){
-            return false;
-        } else {
-            return true;
         }
-
-        //check if startingDateSearch is before ending
+        return true;
     }
-
+    private boolean isValidEntry(){
+        try {
+            if(Double.parseDouble(weightEntry.getText()) < 0 ||  dateEntry.getValue() == null){
+                throw new IllegalArgumentException("Illegal values entered");
+            }
+        } catch(IllegalArgumentException ex) {
+            return false;
+        }
+        return true;
+    }
 }
