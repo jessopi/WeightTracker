4e5fe53d631212c9b3907df1b312ef1bc54b5bb2
jessopi <ianjessop3@gmail.com>
2018-03-19 17:47:06 -0400

    Added sqlite database to package src.main.resources.

--- src/main/java/dbutility/DbConnection.java
+++ src/main/java/dbutility/DbConnection.java
@@ -6,16 +6,16 @@ import java.sql.SQLException;
 
 public class DbConnection {
 
-    private static final String SQCONN = "jdbc:sqlite:trackWeight.sqlite";
+    //private static final String SQCONN = "jdbc:sqlite:src/main/resources/trackWeight.sqlite";
 
     public static Connection getConnection() throws SQLException {
+
         try{
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection(SQCONN);
+            return DriverManager.getConnection("jdbc:sqlite::resource:main/resources/trackWeight.sqlite");
         } catch (ClassNotFoundException e){
             e.printStackTrace();
         }
         return null;
     }
-
 }
